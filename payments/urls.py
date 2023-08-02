4e9b829f4b2bdaef8c411baf51224f64b8227cf3
Jen10Gaia <141237908+Jen10Gaia@users.noreply.github.com>
2023-08-02 18:26:48 +0300

    Added Payments App

---
+++ payments/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+#   path('jobs/', views.getAllJobs, name='jobs'),
+#   path('create-payment-intent/', views.create_payment, name='create_payment'),
+
+    
+]
